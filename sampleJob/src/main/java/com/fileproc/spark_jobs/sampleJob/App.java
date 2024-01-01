7d00be2af70f5fb696c2f9e4df41fd192d35bb6a
Keshore Durairaj <keshore12@gmail.com>
2024-01-02 00:29:44 +0530

    added write methods

--- sampleJob/src/main/java/com/fileproc/spark_jobs/sampleJob/App.java
+++ sampleJob/src/main/java/com/fileproc/spark_jobs/sampleJob/App.java
@@ -1,11 +1,39 @@
 package com.fileproc.spark_jobs.sampleJob;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
 
 public class App {
-	public static void main(String[] args) {
+	public static void DFToCSV(Dataset<Row> df, String filename) throws IOException {
+		String tmpDir = filename + ".tmp";
+		df.coalesce(1)//
+				.write()//
+				.format("csv").mode(SaveMode.Overwrite)//
+				.option("header", "true")//
+				.save(tmpDir);
+
+		String fileName = Stream.of(new File(tmpDir).listFiles())//
+				.filter(file -> file.getName().startsWith("part-0"))//
+				.findFirst().get().getName();
+
+		Files.move(Paths.get(tmpDir + "/" + fileName), Paths.get(filename), StandardCopyOption.REPLACE_EXISTING);
+
+		FileUtils.deleteDirectory(new File(tmpDir));
+
+	}
+
+	public static void main(String[] args) throws Exception {
 		System.out.println("Hello World!");
 
 		SparkSession spark = SparkSession.builder()//
@@ -13,9 +41,19 @@ public class App {
 				.appName("sampleJob")//
 				.getOrCreate();
 
-		Dataset<Row> employees = spark.read()//
+		// RDD
+		// Dataframe
+		// Dataset
+
+		Dataset<Row> employees = spark//
+				.read()//
 				.option("header", true)//
-				.csv("/home/keshore/Desktop/employees.csv");
+				.csv("/home/keshore/Desktop/employees.csv")//
+				.withColumn("age", functions.col("id"))//
+				.withColumn("id", functions.expr("'Hello-' || id"))//
+		;
+
+		DFToCSV(employees, "/home/keshore/Desktop/employees_out.csv");
 
 		employees.printSchema();
 
