c449b27feaf4bb43df79c33a93c844e6d059876d
Keshore Durairaj <keshore12@gmail.com>
2024-01-01 23:09:58 +0530

    sample working version

---
+++ sampleJob/src/main/java/com/mhnasir/spark_jobs/sampleJob/App.java
@@ -0,0 +1,24 @@
+package com.mhnasir.spark_jobs.sampleJob;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		SparkSession spark = SparkSession.builder()//
+				.master("local")//
+				.appName("sampleJob")//
+				.getOrCreate();
+
+		Dataset<Row> employees = spark.read()//
+				.option("header", true)//
+				.csv("/home/keshore/Desktop/employees.csv");
+
+		employees.printSchema();
+
+		employees.show();
+	}
+}
